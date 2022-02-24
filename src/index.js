54b5daf47e73baa868a9ff748240102f08500733
huangyejun <huangyejun@jd.com>
2022-02-24 18:01:03 +0800

    修改路径

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import DemoHtml from './components/demo/DemoHtml'
+import DemoHtml from './components/DemoHtml'
 
 function App(props) {
   return <DemoHtml />
